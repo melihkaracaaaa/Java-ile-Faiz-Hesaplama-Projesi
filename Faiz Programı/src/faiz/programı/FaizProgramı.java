28ee4a4ca7e54cd1cb20e659548800df636548d0
Melihcan Karaca <karacamelihcan1@gmail.com>
2022-09-05 21:13:33 +0300

    Update FaizProgramı.java

--- Faiz Programı/src/faiz/programı/FaizProgramı.java
+++ Faiz Programı/src/faiz/programı/FaizProgramı.java
@@ -1,20 +1,20 @@
 
-package faiz.programı;
+package faiz.programi;
 
 import java.util.Scanner;
 
-public class FaizProgramı {
+public class FaizProgrami {
 
     public static void main(String[] args) {
         
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Bankamıza Hoşgeldiniz... (Faiz oranımız %6) ");
+        System.out.println("Bankamiza Hosgeldiniz... (Faiz oranimiz %6) ");
         
         int anapara,vade;
          
-        System.out.print("Yatırmak İstediğiniz Tutar :");
+        System.out.print("Yatirmak İstediginiz Tutar :");
         anapara = scanner.nextInt();
-        System.out.print("Kaç Yıl Vade İstiyorsunuz :");
+        System.out.print("Kac Yil Vade İstiyorsunuz :");
         vade = scanner.nextInt();
         double toplampara =anapara;
         double faizoranı = 0.06 ;
@@ -22,7 +22,7 @@ public class FaizProgramı {
         for (int i = 1 ; i <= vade ; i++) {
             
             toplampara = (anapara * faizoranı) + toplampara;
-            System.out.println(i + ".Yılın Sonunda : " + "$   " + toplampara  );
+            System.out.println(i + ".Yilin Sonunda : " + "$   " + toplampara  );
             
         }
         
